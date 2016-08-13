7ee7e89cd2bbd9753c1ce9531333949ead1470c4
niketarachhadia <niketa.rachhadia@gmail.com>
2016-08-13 14:32:21 -0500

    top Answerer commit

--- js/app.js
+++ js/app.js
@@ -31,26 +31,62 @@ var showQuestion = function(question) {
 	return result;
 };
 
+var showAnswerer = function(answerer){
+	var result = $('.templates .answerer').clone();
+	var answererName = result.find('.answerer-name a');
+	answererName.attr('href', answerer.user.link);
+	answererName.text(answerer.user.display_name);
 
-// this function takes the results object from StackOverflow
-// and returns the number of results and tags to be appended to DOM
+	var  count = result.find('.post-count');
+	count.text(answerer.post_count);
+
+	var  score = result.find('.score');
+	score.text(answerer.score);
+
+	var  reputation = result.find('.reputation');
+	reputation.text(answerer.user.reputation);
+
+	return result;
+};
+
 var showSearchResults = function(query, resultNum) {
 	var results = resultNum + ' results for <strong>' + query + '</strong>';
 	return results;
 };
 
-// takes error string and turns it into displayable DOM element
 var showError = function(error){
 	var errorElem = $('.templates .error').clone();
 	var errorText = '<p>' + error + '</p>';
 	errorElem.append(errorText);
 };
 
-// takes a string of semi-colon separated tags to be searched
-// for on StackOverflow
+var getTopAnswerers = function(tag){
+	var request = {
+		site : 'stackoverflow',
+	};
+	$.ajax({
+		url: "http://api.stackexchange.com/2.2/tags/"+tag+"/top-answerers/month",
+		data : request,
+		dataType : 'jsonp',
+		type : 'GET'
+
+	}).done(function(result){
+		var searchResults = showSearchResults(tag,result.items.length);
+		console.log(searchResults);
+		console.log(result.items);
+		$('.search-results').html(searchResults);
+		$.each(result.items, function(i, item) {
+			var answerer = showAnswerer(item);
+			$('.results').append(answerer);
+		});
+	}).fail(function(jqXHR,error){
+		var errorElem = showError(error);
+		$('.search-results').append(errorElem);
+	});
+};
+
 var getUnanswered = function(tags) {
-	
-	// the parameters we need to pass in our request to StackOverflow's API
+
 	var request = { 
 		tagged: tags,
 		site: 'stackoverflow',
@@ -61,15 +97,13 @@ var getUnanswered = function(tags) {
 	$.ajax({
 		url: "http://api.stackexchange.com/2.2/questions/unanswered",
 		data: request,
-		dataType: "jsonp",//use jsonp to avoid cross origin issues
+		dataType: "jsonp",
 		type: "GET",
 	})
 	.done(function(result){ //this waits for the ajax to return with a succesful promise object
 		var searchResults = showSearchResults(request.tagged, result.items.length);
 
 		$('.search-results').html(searchResults);
-		//$.each is a higher order function. It takes an array and a function as an argument.
-		//The function is executed once for each item in the array.
 		$.each(result.items, function(i, item) {
 			var question = showQuestion(item);
 			$('.results').append(question);
@@ -90,5 +124,16 @@ $(document).ready( function() {
 		// get the value of the tags the user submitted
 		var tags = $(this).find("input[name='tags']").val();
 		getUnanswered(tags);
+		});
+	$('.inspiration-getter').submit( function(e){
+		e.preventDefault();
+		// zero out results if previous search has run
+		$('.results').html('');
+		// get the value of the tags the user submitted
+		// var tags = $(this).find("input[name='tags']").val();
+		// getUnanswered(tags);
+		var tag = $(this).find("input[name='answerers']").val();
+		getTopAnswerers(tag);
 	});
+
 });
